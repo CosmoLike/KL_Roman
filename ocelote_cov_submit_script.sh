d1e5549fa94bf6bb5444292c4b9bf435d7d2f7d6
Jiachuan Xu <jiachuanxu@login3.cm.cluster>
2019-09-06 10:58:20 -0700

    customize ocelote_cov_submit_script.sh

--- ocelote_cov_submit_script.sh
+++ ocelote_cov_submit_script.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #PBS -S /bin/bash
 #PBS -V
-#PBS -W group_list=cosmolike
+#PBS -W group_list=cosmo
 #PBS -q standard
 #PBS -J 1-8365
 #PBS -l select=1:ncpus=1:mem=1GB
 #PBS -l place=free:shared
 #PBS -l walltime=8:00:00
-#PBS -N W1st_cov
-#PBS -e /home/u17/timeifler/output/
-#PBS -o /home/u17/timeifler/output/
+#PBS -N KL_W1st_cov
+#PBS -e /home/u17/jiachuanxu/output/
+#PBS -o /home/u17/jiachuanxu/output/
 
 module load gsl/2/2.1
 
 cd $PBS_O_WORKDIR
-/home/u17/timeifler/CosmoLike/WFIRST_forecasts/./compute_covariances_fourier $PBS_ARRAY_INDEX >& /home/u17/timeifler/output/job_output_$PBS_ARRAY_INDEX.log
+/home/u17/jiachuanxu/CosmoLike/KL_WFIRST/./compute_covariances_fourier $PBS_ARRAY_INDEX >& /home/u17/jiachuanxu/output/job_output_$PBS_ARRAY_INDEX.log
 
 
 
