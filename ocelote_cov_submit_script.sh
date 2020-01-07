0a96fab367655e0e6f126797a791f84d2a74c372
Jiachuan Xu <miss199602@163.com>
2020-01-07 09:09:45 -0700

    Update covariance script params.

--- ocelote_cov_submit_script.sh
+++ ocelote_cov_submit_script.sh
@@ -3,18 +3,22 @@
 #PBS -V
 #PBS -W group_list=cosmo
 #PBS -q standard
-#PBS -J 1-1540
+#PBS -J 1-7223
 #PBS -l select=1:ncpus=1:mem=1GB
 #PBS -l place=free:shared
 #PBS -l walltime=8:00:00
-#PBS -N WL_W1st_KL_cov
+#PBS -N KL_W1st_30_cov
 #PBS -e /home/u17/jiachuanxu/output/
 #PBS -o /home/u17/jiachuanxu/output/
 
 module load gsl/2/2.1
 
 cd $PBS_O_WORKDIR
-/home/u17/jiachuanxu/CosmoLike/KL_WFIRST/./compute_covariances_fourier $PBS_ARRAY_INDEX >& /home/u17/jiachuanxu/output/job_output_$PBS_ARRAY_INDEX.log
+for (( c=0; c<15; c++ ))
+do
+	hit=$(( $PBS_ARRAY_INDEX + $c * 7223 ))
+	/home/u17/jiachuanxu/CosmoLike/KL_WFIRST/./compute_covariances_fourier $hit  >& /home/u17/jiachuanxu/output/job_output_$hit.log
+done
 
 
 
