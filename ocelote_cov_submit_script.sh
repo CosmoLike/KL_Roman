889716573e275093faeb2e4189fa77a8419883e3
Jiachuan Xu <miss199602@163.com>
2020-01-30 23:24:59 -0700

    update job scripts

--- ocelote_cov_submit_script.sh
+++ ocelote_cov_submit_script.sh
@@ -3,10 +3,10 @@
 #PBS -V
 #PBS -W group_list=cosmo
 #PBS -q standard
-#PBS -J 1-3495
-#PBS -l select=1:ncpus=1:mem=1GB
+#PBS -J 1-1540
+#PBS -l select=1:ncpus=1:mem=6GB
 #PBS -l place=free:shared
-#PBS -l walltime=8:00:00
+#PBS -l walltime=6:00:00
 #PBS -N KL_W1st_30_cov
 #PBS -e /home/u17/jiachuanxu/output/
 #PBS -o /home/u17/jiachuanxu/output/
@@ -14,11 +14,12 @@
 module load gsl/2/2.1
 
 cd $PBS_O_WORKDIR
-for (( c=0; c<31; c++ ))
-do
-	hit=$(( $PBS_ARRAY_INDEX + $c * 3495 ))
-	/home/u17/jiachuanxu/CosmoLike/KL_WFIRST/./compute_covariances_fourier $hit  >& /home/u17/jiachuanxu/output/job_output_$hit.log
-done
+#for (( c=0; c<233; c++ ))
+#do
+#	hit=$(( $PBS_ARRAY_INDEX + $c * 465 ))
+#	/home/u17/jiachuanxu/CosmoLike/KL_WFIRST/./compute_covariances_fourier $hit
+#done
+/home/u17/jiachuanxu/CosmoLike/KL_WFIRST/./compute_covariances_fourier $PBS_ARRAY_INDEX
 
 
 
