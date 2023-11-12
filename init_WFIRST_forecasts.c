d6ee4811a32b69bc3c9f3feafff9fe11ef5aadff
wolfrayet <yvonne.shenlong@gmail.com>
2023-11-11 17:54:12 -0700

    include DSA in prior

--- init_WFIRST_forecasts.c
+++ init_WFIRST_forecasts.c
@@ -41,6 +41,7 @@ void set_clphotoz_LSST_Y1();
 void set_wlphotoz_LSST_Y10();
 void set_clphotoz_LSST_Y10();
 void set_wlphotoz_DSA_allsky();
+void set_clphotoz_DSA_allsky();
 
 void set_shear_priors_WFIRST_KL();
 void set_shear_priors_DESI2_KL();
@@ -317,6 +318,10 @@ void init_priors_IA_bary(char *Prior1, char *Prior2, char *Prior3, char *Prior4,
     set_wlphotoz_DESI2_KL();
     set_clphotoz_DESI2_KL();
   }
+  if(strcmp(Prior1,"spec_DSA")==0){
+    set_wlphotoz_DSA_allsky();
+    set_clphotoz_DSA_allsky();
+  }
   // Initializing shear calibration priors
   if(strcmp(Prior2,"shear_opti")==0){
     set_shear_priors_WFIRST_opti();
@@ -335,6 +340,9 @@ void init_priors_IA_bary(char *Prior1, char *Prior2, char *Prior3, char *Prior4,
   }
   if(strcmp(Prior2,"shear_KL_DESI2")==0){
     set_shear_priors_DESI2_KL();
+  }
+  if(strcmp(Prior2,"shear_DSA_allsky")==0){
+    set_shear_priors_DSA_allsky();
   }
   if(strcmp(Prior3,"GRS")==0) like.GRS=1;
   // external probes
@@ -442,6 +450,10 @@ void init_priors_IA(char *Prior1, char *Prior2, char *Prior3, char *Prior4,
     set_wlphotoz_DESI2_KL();
     set_clphotoz_DESI2_KL();
   }
+  if(strcmp(Prior1,"spec_DSA")==0){
+    set_wlphotoz_DSA_allsky();
+    set_clphotoz_DSA_allsky();
+  }
   // Initializing shear calibration priors
   if(strcmp(Prior2,"shear_opti")==0){
     set_shear_priors_WFIRST_opti();
@@ -454,6 +466,9 @@ void init_priors_IA(char *Prior1, char *Prior2, char *Prior3, char *Prior4,
   }
   if(strcmp(Prior2,"shear_KL_DESI2")==0){
     set_shear_priors_DESI2_KL();
+  }
+  if(strcmp(Prior2,"shear_DSA_allsky")==0){
+    set_shear_priors_DSA_allsky();
   }
   if(strcmp(Prior3,"GRS")==0) like.GRS=1;
   // external probes
@@ -529,12 +544,17 @@ void init_priors_KL(char *Prior1, char *Prior2, char *Prior3, char *Prior4)
     set_wlphotoz_DESI2_KL();
     set_clphotoz_DESI2_KL();
   }
+  if(strcmp(Prior1,"spec_DSA")==0){}
   if(strcmp(Prior2,"shear_KL_WFIRST")==0){
     set_shear_priors_WFIRST_KL();
   }
   if(strcmp(Prior2,"shear_KL_DESI2")==0){
     set_shear_priors_DESI2_KL();
   }
+  if(strcmp(Prior2,"shear_KL_DSA")==0){
+    set_shear_priors_DSA_allsky();
+  }
+
   if(strcmp(Prior3,"GRS")==0) like.GRS=1;
   // external probes
   sprintf(like.ext_data,"%s",Prior4);
@@ -572,6 +592,7 @@ void init_survey(char *surveyname)
   if(strcmp(surveyname,"WFIRST_WL")==0) set_survey_parameters_to_WFIRST_WL();
   if(strcmp(surveyname,"WFIRST_KL")==0) set_survey_parameters_to_WFIRST_KL();
   if(strncmp(surveyname,"DESI2_KL",8)==0) set_survey_parameters_to_DESI2_KL(surveyname);
+  if(strncmp(surveyname,"DSA_allsky")==0) set_survey_parameters_to_DSA_allsky();
 
   printf("Survey set to %s\n",survey.name);
   printf("Survey area: %f deg^2\n",survey.area);
@@ -1569,7 +1590,7 @@ void set_wlphotoz_DSA_allsky()
 {
   int i;
   printf("\n");
-  printf("Source sample: DSA All-sky spec-z uncertainty initialized\n");
+  printf("Source sample: DSA all-sky spec-z uncertainty initialized\n");
   for (i=0;i<tomo.shear_Nbin; i++){
     nuisance.bias_zphot_shear[i]=0.0;
     nuisance.sigma_zphot_shear[i]=0.0002; 
@@ -1586,14 +1607,35 @@ void set_wlphotoz_DSA_allsky()
   }
   like.wlphotoz=1;
 }
+void set_clphotoz_DESI2_KL()
+{
+  int i;
+  printf("\n");
+  printf("Lens sample: DSA all-sky KL spec-z uncertainty initialized\n");
+  for (i=0;i<tomo.clustering_Nbin; i++){
+    nuisance.bias_zphot_clustering[i]=0.0;
+    nuisance.sigma_zphot_clustering[i]=0.0002; 
+    printf("nuisance.bias_zphot_clustering[%d]=%le\n",i,nuisance.bias_zphot_clustering[i]);
+    printf("nuisance.sigma_zphot_clustering[%d]=%le\n",i,nuisance.sigma_zphot_clustering[i]);
+    // center of Gaussian priors
+    prior.bias_zphot_clustering[i][0]=nuisance.bias_zphot_clustering[i];
+    prior.sigma_zphot_clustering[i][0]=nuisance.sigma_zphot_clustering[i];
+    // rms width of Gaussian priors
+    prior.bias_zphot_clustering[i][1] = 0.00004;
+    prior.sigma_zphot_clustering[i][1]= 0.00004;
+    printf("Mean (of mean)=%le, Sigma (of mean)=%le\n",prior.bias_zphot_clustering[i][0],prior.bias_zphot_clustering[i][1]);
+    printf("Mean (of sigma)=%le, Sigma (of sigma)=%le\n",prior.sigma_zphot_clustering[i][0],prior.sigma_zphot_clustering[i][1]); 
+  }
+  like.clphotoz=1;
+}
 void set_shear_priors_DSA_allsky() 
 {
-  // copy from LSST Y1
+  // copy from DESI-II
   int i;
   printf("\nSetting Gaussian shear calibration Priors DSA All-sky\n");
   for (i=0;i<tomo.shear_Nbin; i++){
     prior.shear_calibration_m[i][0] = 0.0;
-    prior.shear_calibration_m[i][1] = 0.013;
+    prior.shear_calibration_m[i][1] = 0.0004;
     printf("Mean=%le, Sigma=%le\n",prior.shear_calibration_m[i][0],prior.shear_calibration_m[i][1]);
   }
   like.shearcalib=1;
