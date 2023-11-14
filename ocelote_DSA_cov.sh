62ffc8dcc6c0c455d379769a68fe3d941928f2d3
wolfrayet <yvonne.shenlong@gmail.com>
2023-11-14 09:54:10 -0700

    script for DSA

---
+++ ocelote_DSA_cov.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=covDSA
+###SBATCH --output=covDSA-%A_%a.out
+#SBATCH --array=1-770
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=high_priority
+#SBATCH --qos=user_qos_timeifler
+#SBATCH --account=timeifler
+#SBATCH --time=0:20:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=yhhuang@arizona.edu
+
+module load gsl
+module load openmpi3
+WORKDIR=/home/u15/yhhuang/cosmology/CosmoLike/KL_WFIRST
+cd ${WORKDIR}
+for (( c=0; c<4; c++ ))
+do
+	hit=$(( ${SLURM_ARRAY_TASK_ID} + c * 770 ))
+	./compute_covariances_fourier ${hit}
+done
+
+
+
