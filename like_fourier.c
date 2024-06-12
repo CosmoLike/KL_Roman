b0698fced6e23f68265671cfac73243fc51de16d
wolfrayet <yvonne.shenlong@gmail.com>
2024-06-12 15:55:56 -0700

    fix the coefficient for one component correlation

--- like_fourier.c
+++ like_fourier.c
@@ -134,7 +134,7 @@ void set_data_shear(int Ncl, double *ell, double *data, int start, int one)
     for (i = 0; i < Ncl; i++){
       if (ell[i] < like.lmax_shear){ 
         if (one == 1) {
-          data[Ncl*nz+i] = 0.5*C_shear_tomo_sys(ell[i],z1,z2);
+          data[Ncl*nz+i] = 0.25*C_shear_tomo_sys(ell[i],z1,z2);
         }
         else {
           data[Ncl*nz+i] = C_shear_tomo_sys(ell[i],z1,z2);
@@ -806,7 +806,7 @@ int main(int argc, char** argv)
   int one = 1;                                   // enable sigal componenet
   int photoz_flag = 0;                           // enable two photoz uncertainties
   char dndz[1][100] = {"zdistris/zdistri_SKA_KL"};  // redshift distribution
-  int Ntomo_source = 1;
+  int Ntomo_source = 10;
   printf("%d target selection scenarios\n", N_scenarios_selection);
   // 6 sets of shape noise, used to refer to covariance matrix only
   // detailed settings are stored in `set_survey_parameters_to_DESI2_KL()`
@@ -815,7 +815,7 @@ int main(int argc, char** argv)
   // Lens galaxies not used, set to random value
   double lens_density = 66.0;
   // Lens galaxies not used, set to random value
-  int Ntomo_lens = 1;
+  int Ntomo_lens = 10;
   double Rmin_bias = 21.0; // not used 
   // 15 ell bins in Fourier space, from 20 to 3000
   int Nell = 15;
