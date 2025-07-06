025364e26fa84ec841db11c9e065f7d6ad249b10
wolfrayet <yvonne.shenlong@gmail.com>
2025-07-05 17:10:48 -0700

    update inv_cov to plot full covariance

--- inv_cov.py
+++ inv_cov.py
@@ -1,205 +1,240 @@
 #!/usr/bin/python
-import sys
-import math, numpy as np
+import sys, os
+import math
+import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from numpy import linalg as LA
-import numpy as np
 
-# covariance matrix
-infile =['Total_Cov_Matrix/WFIRST_WL_Ngal51.00_Area2000.00_ssss_cov_Ncl20_Ntomo10_sigmae0.37_baseline']
-#infile =['/users/timeifler/Dropbox/cosmolike_store/WFIRST_forecasts/cov/cov_WFIRST_Ncl25_4clusterbins_nrichmin25_source_Dec17']
+def parse_ini_file(fname):
+	params = {}
+	with open(fname, 'r') as f:
+		for line in f:
+			line = line.strip()
+			if not line or line.startswith('#'):
+				continue
+			if ':' in line:
+				key, value = map(str.strip, line.split(':', 1))
+				try:
+					params[key] = int(value)
+				except ValueError:
+					try:
+						params[key] = float(value)
+					except ValueError:
+						params[key] = value
+	return params
 
-# data vector
-data= ['/home/u17/jiachuanxu/CosmoLike/KL_WFIRST/datav/WFIRST_shear_shear_opti_Ntomo10_Ncl20_sigmae0.37_dmo_ia']
-
-# output file name
-outname=['WFIRST_WL_Ntomo10_Ncl20_sigmae0.37_baseline']
-
-# the numbers below can be computed knowing the data vector settings, e.g. 10 tomographic source bins results in 55 shear-shear power spectra. Or they can be read off when running the covariance code, i.e. type 'compute_covariance_fourier 100000' and look for the output mentioning number of ggl bins accepted and/or number of cluster weka lensing bins accepted. The default numbers below most likely don't correspond to your binning choices.
-nggl = 0 	# number of ggl power spectra
-ngcl = 0	# number of cluster-source galaxy power spectra
-nlens = 0 	# number of lens bins 
-nlenscl= 0 	# number of cluster redshift bins 
-nshear = 55 	# number of shear tomographic power spectra
-ncl=20		# number of ell-bins
-nclgcl=0	# number of cluster ell-bins
-nrich=0		# number of richness bins
+params = parse_ini_file('params.ini')
+Ncl = params.get('Ncl')
+Ntomo = params.get('Ntomo_source')
+Area = params.get('survey_area')
+Nsrc = params.get('n_gal')
+ShapeNoise = 0.05
+ellmax = params.get('lmax')
 
 
-ndata = (nshear+nggl+nlens)*ncl+nlenscl*nrich+nrich*ngcl*nclgcl
-n2pt = (nshear+nggl+nlens)*ncl 
-ncluster = nlenscl*nrich 
-n2ptcl=n2pt+ncluster
-nclusterN_WL=ncluster+nrich*ngcl*nclgcl
+infile_fmt = 'cov/Roman_KL_%s_cov_Ncl%d_Ntomo%d'
+outfile_fmt = 'invcov/Roman_KL_3x2pt_invcov_Ncl%d_Ntomo%d'
+DATA_DIR = '../3Dx2D/'
+probes_list = ['ssss', 'llll', 'lsls', 'llls', 'lsss', 'llss']
 
-for k in range(0,1):
-  	datafile= np.genfromtxt(data[k])
-  	mask = np.zeros(ndata)
-	for i in range(0,datafile.shape[0]):
-		if (datafile[i,1] >1.0e-15): 
-			mask[i]=1.0
-#	for m in mask:
-#		m = 1.0
-  	
-  	covfile = np.genfromtxt(infile[k])
-	cov = np.zeros((ndata,ndata))
+# the numbers below can be computed knowing the data vector settings, 
+# e.g. 10 tomographic source bins results in 55 shear-shear power spectra. 
+# Or they can be read off when running the covariance code, i.e. type 'compute_covariance_fourier 100000' 
+# and look for the output mentioning number of ggl bins accepted and/or 
+# number of cluster weak lensing bins accepted. 
+# The default numbers below most likely don't correspond to your binning choices.
+nggl 	= 0 						# number of ggl power spectra
+nlens 	= int(Ntomo) 				# number of lens bins 
+nshear 	= int((Ntomo+1)*Ntomo/2) 	# number of shear tomographic power spectra
+nlenscl = 0 						# number of cluster redshift bins 
+ncl 	= int(Ncl)					# number of ell-bins
+nclgcl 	= 0							# number of cluster ell-bins
+ngcl 	= 0							# number of cluster-source galaxy power spectra
+nrich 	= 0							# number of richness bins
 
-	print ndata,n2pt,int(np.max(covfile[:,0])+1)
+# set data vector dimensions
+ndata 	= int((nshear+nggl+nlens)*ncl + nlenscl*nrich + nrich*ngcl*nclgcl)
+n2pt 	= int((nshear+nggl+nlens)*ncl)
+ncluster = int(nlenscl*nrich)
+n2ptcl = int(n2pt+ncluster)
+nclusterN_WL = int(ncluster+nrich*ngcl*nclgcl)
+print(f'Data Vector Size = {ndata}')
 
-	for i in range(0,covfile.shape[0]):
-	  	cov[int(covfile[i,0]),int(covfile[i,1])] = covfile[i,8]+covfile[i,9] # accounting both gaussian and ng
-	  	cov[int(covfile[i,1]),int(covfile[i,0])] = covfile[i,8]+covfile[i,9]
-		# cov[int(covfile[i,0]),int(covfile[i,1])] = covfile[i,8]
-	 	# cov[int(covfile[i,1]),int(covfile[i,0])] = covfile[i,8]
-	 
+# use all-1 mask for new
+mask = np.ones(ndata)
+
+# figure format
+plot_corrmat = False	# True for correlation matrix, False for covariance matrix
+fig_title = "$n_\mathrm{src}=$%.0f arcmin$^{-2}$, $\ell_\mathrm{max}$=%.0f"%(Nsrc, ellmax)
+fig_filename = "../3Dx2D/figure/Roman_KL_3x3pt_cov_Ncl%d_Ntomo%d.png"%(Ncl, Ntomo)
+fontsize = 10
+
+cov = np.zeros((ndata, ndata))
+for probe in probes_list:
+	infile = DATA_DIR + infile_fmt%(probe, Ncl, Ntomo)
+	if not os.path.exists(infile):
+		print(f'File {infile} does not exist! Continue')
+		continue
+
+	covfile = np.genfromtxt(infile)
+	for l in range(0, covfile.shape[0]):
+		bin1, bin2 = int(covfile[l,0]), int(covfile[l,1])
+		# normal format: g/ng
+		if covfile.shape[1] == 10:
+			cov_g, cov_ng = covfile[l,8], covfile[l,9]
+		# break-down version
+		elif covfile.shape[1] == 12:
+			cov_g = covfile[l,8] + covfile[l,9] + covfile[l,10]
+			cov_ng = covfile[l,11]
+		# covariance only
+		elif covfile.shape[1] == 3:
+			cov_g = 0; cov_ng = covfile[l,2]
+		else:
+			print(f'Wrong covariance matrix file format! {covfile.shape[1]} columns')
+			exit(-1)
+		
+		if bin1 == bin2:
+			factor = 1
+		else:
+			factor = mask[bin1] * mask[bin2]
+		cov[bin1, bin2] += cov_g * factor
+		cov[bin2, bin1] += cov_g * factor
+# correlation matrix
+cor = cov / np.outer(np.diagonal(cov)**0.5, np.diagonal(cov)**0.5)
+a = np.sort(LA.eigvals(cor[:,:]))
+print("Eigenvalues range of corrmat (%.3e, %.3e)"%(np.min(a), np.max(a)))
+print("Negative eigenvalues of corrmat:{}".format(a[a<0]))
+
+############### invert 2pt covariance #################
+a = np.sort(LA.eigvals(cov[0:n2pt,0:n2pt]))
+print("min+max eigenvalues 2pt cov:")
+print(np.min(a), np.max(a))
+
+inv = LA.inv(cov[0:n2pt,0:n2pt])
+outfile = DATA_DIR + outfile_fmt%(Ncl, Ntomo)
+f = open(outfile, "w")
+for i in range(0,n2pt):
+	inv[i,i]=inv[i,i]*mask[i]
+	for j in range(0,n2pt):
+		f.write("%d %d %e\n" %( i,j, inv[i,j]))
+f.close()
+
+# ############### invert shear covariance #################
+# inv = LA.inv(cov[0:nshear*ncl,0:nshear*ncl])
+# a = np.sort(LA.eigvals(cov[0:nshear*ncl,0:nshear*ncl]))
+# print("Eigvals range of covmat (%.3e, %.3e)"%(np.min(a), np.max(a)))
+# f = open(outname, 'w')
+# for i in range(0,nshear*ncl):
+# 	for j in range(0,nshear*ncl):
+# 		f.write("%d %d %e\n" %(i, j, inv[i,j]*mask[i]*mask[j]))
+# f.close()
+
+# ############### invert clustering covariance #################
+# inv = LA.inv(cov[(nshear+nggl)*ncl:(nshear+nggl+nlens)*ncl,(nshear+nggl)*ncl:(nshear+nggl+nlens)*ncl])
+# a = np.sort(LA.eigvals(cov[(nshear+nggl)*ncl:(nshear+nggl+nlens)*ncl,(nshear+nggl)*ncl:(nshear+nggl+nlens)*ncl]))
+# print("min+max eigenvalues clustering cov:")
+# print(np.min(a), np.max(a))
+# outfile = "cov/"+outname[k]+"_pos_pos_inv"
+# f = open(outfile, "w")
+# for i in range(0,nlens*ncl):
+# 	inv[i,i]=inv[i,i]*mask[(nshear+nggl)*ncl+i]
+# 	for j in range(0,nlens*ncl):
+# 		f.write("%d %d %e\n" %(i,j, inv[i,j]))
+# f.close()
+
+# ############### invert full2pt+clusterN+clusterWL covariance #################
+# precond = 1.e-7
+# for i in range(0,ncluster):
+# 	cov[n2pt+i,:]*= precond
+# 	cov[:,n2pt+i]*= precond
+# inv = LA.inv(cov)
+# a = np.sort(LA.eigvals(cov))
+# print "min+max eigenvalues of full 2ptclusterN+clusterWL pre-conditioned matrix:"
+# print np.min(a), np.max(a)
+# if (np.min(a)<0):
+# 	print "WARNING  WARNING: %s is not positive definite! WARNING!" % (infile[k])
+# for i in range(0,ncluster):
+# 	inv[n2pt+i,:]*= precond
+# 	inv[:,n2pt+i]*= precond
+
+# outfile = "cov/"+outname[k]+"_3x2pt_clusterN_clusterWL_inv"
+# f = open(outfile, "w")
+# for i in range(0,ndata):
+# 	inv[i,i]=inv[i,i]*mask[i]
+# 	for j in range(0,ndata):
+# 	f.write("%d %d %e\n" %( i,j, inv[i,j]))
+# f.close()
+
+
+
+# ############### invert clusterN+clusterWL covariance #################
+# inv = LA.inv(cov[n2pt:n2pt+nclusterN_WL,n2pt:n2pt+nclusterN_WL])
+# a = np.sort(LA.eigvals(cov[n2pt:n2pt+nclusterN_WL,n2pt:n2pt+nclusterN_WL]))
+# print "min+max eigenvalues of clusterN_WL pre-conditioned matrix:"
+# print np.min(a), np.max(a)
+# if (np.min(a)<0):
+# 	print "WARNING  WARNING: %s is not positive definite! WARNING!" % (infile[k])
+
+# for i in range(0,ncluster):
+# 	inv[i,:]*= precond
+# 	inv[:,i]*= precond
+
+# outfile = "cov/"+outname[k]+"_clusterN_clusterWL_inv"
+# f = open(outfile, "w")
+# for i in range(0,nclusterN_WL):
+# 	inv[i,i]=inv[i,i]*mask[n2pt+i]
+# 	for j in range(0,nclusterN_WL):
+# 		f.write("%d %d %e\n" %( i,j, inv[i,j]))
+# f.close()
+
+### Plot correlation matrix or covariance matrix
+
+labels = [
+r'$C^{\kappa \kappa}\left(\ell,z_{\mathrm{s}_i},z_{\mathrm{s}_j}\right)$',
+r'$C^{g \kappa}\left(\ell,z_{\mathrm{s}_i},z_{\mathrm{s}_j}\right)$',
+r'$C^{gg}\left(\ell,z_{\mathrm{s}_i},z_{\mathrm{s}_j}\right)$',
+# r'$N^{c}\left(r,z_{\mathrm{c}_i}\right)$',
+# r'$C^{c \kappa}\left(\ell,z_{\mathrm{s}_i},z_{\mathrm{s}_j}\right)$',
+]
+ticks = np.zeros(4)
+ticks[1] = nshear*ncl
+ticks[2] = (nshear+nggl)*ncl
+ticks[3] = n2pt
+# ticks[4] = n2pt+ncluster
+# ticks[5] = ndata
+tickx = 0.5*(ticks[:-1]+ticks[1:])
+
+fig = plt.figure(figsize=(10,10))
+ax = plt.gca()
+if plot_corrmat:
+	cor = cov/np.outer(np.diagonal(cov)**0.5, np.diagonal(cov)**0.5)
+	cor = cor*np.outer(mask, mask)
+
+	im = ax.imshow(cor, interpolation='nearest', origin='lower', vmin=-1, vmax=1,
+					cmap='seismic')
+else:
+	im = ax.imshow(np.log10(cov), interpolation='nearest', origin='lower', vmin=-25, vmax=-10,
+					cmap='seismic')
+
 	
-	cor = np.zeros((ndata,ndata))
-	for i in range(0,ndata):
-	    for j in range(0,ndata):
-	    	if (cov[i,i]*cov[j,j] >0):
-	       		cor[i,j] = cov[i,j]/math.sqrt(cov[i,i]*cov[j,j])
+	# for i in range(0,5):
+	# 	tickx[i] = 0.5*(ticks[i]+ticks[i+1])
+	# 	plt.plot([ticks[i]-0.5,ticks[i]-0.5],[-.5,ndata-0.5],linestyle ='-',color = 'k')
+	# 	plt.plot([-.5,ndata-0.5],[ticks[i]-0.5,ticks[i]-0.5],linestyle ='-',color = 'k')
+ax.set_title(fig_title, fontsize=16)
+plt.xticks(tickx, labels, fontsize=fontsize)
+plt.yticks(tickx-0.5, labels, fontsize=fontsize)
+#plt.tick_params(axis = 'x',length = 0, pad = 15)
+#plt.tick_params(axis = 'y',length = 0, pad = 5)
+
+plt.colorbar(im)
+plt.savefig(fig_filename, format="png")
+plt.close()
 
 
-	a = np.sort(LA.eigvals(cor[:,:]))
-	print "min+max eigenvalues full cor:"
-	print np.min(a), np.max(a)
-	print "neg eigenvalues full cor:"
-	for i in range(0,a.shape[0]):
-		if (a[i]< 0.0): print a[i]
-
-
-	# ############### invert shear covariance #################
-	inv = LA.inv(cov[0:nshear*ncl,0:nshear*ncl])
-	a = np.sort(LA.eigvals(cov[0:nshear*ncl,0:nshear*ncl]))
-	print "min+max eigenvalues shear cov:"
-	print np.min(a), np.max(a)
-	outfile = "cov/"+outname[k]+"_shear_shear_inv"
-	f = open(outfile, "w")
-	for i in range(0,nshear*ncl):
-		inv[i,i]=inv[i,i]*mask[i]
-	  	for j in range(0,nshear*ncl):
-	  		f.write("%d %d %e\n" %(i,j, inv[i,j]))
-	f.close()
-	'''	
-	# ############### invert clustering covariance #################
-	inv = LA.inv(cov[(nshear+nggl)*ncl:(nshear+nggl+nlens)*ncl,(nshear+nggl)*ncl:(nshear+nggl+nlens)*ncl])
-	a = np.sort(LA.eigvals(cov[(nshear+nggl)*ncl:(nshear+nggl+nlens)*ncl,(nshear+nggl)*ncl:(nshear+nggl+nlens)*ncl]))
-	print "min+max eigenvalues clustering cov:"
-	print np.min(a), np.max(a)
-	outfile = "cov/"+outname[k]+"_pos_pos_inv"
-	f = open(outfile, "w")
-	for i in range(0,nlens*ncl):
-		inv[i,i]=inv[i,i]*mask[(nshear+nggl)*ncl+i]
-		for j in range(0,nlens*ncl):
-	  		f.write("%d %d %e\n" %(i,j, inv[i,j]))
-	f.close()
-
-	# ############### invert 2pt covariance #################
-	a = np.sort(LA.eigvals(cov[0:n2pt,0:n2pt]))
-	print "min+max eigenvalues 2pt cov:"
-	print np.min(a), np.max(a)
-	inv = LA.inv(cov[0:n2pt,0:n2pt])
-	outfile = "cov/"+outname[k]+"_3x2pt_inv" 
-	f = open(outfile, "w")
-	for i in range(0,n2pt):
-		inv[i,i]=inv[i,i]*mask[i]
-	  	for j in range(0,n2pt):
-	  		f.write("%d %d %e\n" %( i,j, inv[i,j]))
-	f.close()
-
-
-
-	# # ############### invert full2pt+clusterN+clusterWL covariance #################
-	precond = 1.e-7
-	for i in range(0,ncluster):
-	  cov[n2pt+i,:]*= precond
-	  cov[:,n2pt+i]*= precond
-	inv = LA.inv(cov)
-	a = np.sort(LA.eigvals(cov))
-	print "min+max eigenvalues of full 2ptclusterN+clusterWL pre-conditioned matrix:"
-	print np.min(a), np.max(a)
-	if (np.min(a)<0):
-	  print "WARNING  WARNING: %s is not positive definite! WARNING!" % (infile[k])
-	for i in range(0,ncluster):
-	  inv[n2pt+i,:]*= precond
-	  inv[:,n2pt+i]*= precond
-
-	outfile = "cov/"+outname[k]+"_3x2pt_clusterN_clusterWL_inv"
-	f = open(outfile, "w")
-	for i in range(0,ndata):
-	  inv[i,i]=inv[i,i]*mask[i]
-	  for j in range(0,ndata):
-	    f.write("%d %d %e\n" %( i,j, inv[i,j]))
-	f.close()
-
-
-
-	# # ############### invert clusterN+clusterWL covariance #################
-	inv = LA.inv(cov[n2pt:n2pt+nclusterN_WL,n2pt:n2pt+nclusterN_WL])
-	a = np.sort(LA.eigvals(cov[n2pt:n2pt+nclusterN_WL,n2pt:n2pt+nclusterN_WL]))
-	print "min+max eigenvalues of clusterN_WL pre-conditioned matrix:"
-	print np.min(a), np.max(a)
-	if (np.min(a)<0):
-	  print "WARNING  WARNING: %s is not positive definite! WARNING!" % (infile[k])
-
-	for i in range(0,ncluster):
-	  inv[i,:]*= precond
-	  inv[:,i]*= precond
-
-	outfile = "cov/"+outname[k]+"_clusterN_clusterWL_inv"
-	f = open(outfile, "w")
-	for i in range(0,nclusterN_WL):
-	  	inv[i,i]=inv[i,i]*mask[n2pt+i]
-	  	for j in range(0,nclusterN_WL):
-	  		f.write("%d %d %e\n" %( i,j, inv[i,j]))
-	f.close()
-	'''
-	labels = (r'$C^{\kappa \kappa}\left(\ell,z_{\mathrm{s}_i},z_{\mathrm{s}_j}\right)$',r'$C^{\kappa \kappa}\left(\ell,z_{\mathrm{s}_i},z_{\mathrm{s}_j}\right)$',r'$C^{\kappa \kappa}\left(\ell,z_{\mathrm{s}_i},z_{\mathrm{s}_j}\right)$',r'$C^{\kappa \kappa}\left(\ell,z_{\mathrm{s}_i},z_{\mathrm{s}_j}\right)$',r'$C^{\kappa \kappa}\left(\ell,z_{\mathrm{s}_i},z_{\mathrm{s}_j}\right)$')
-	ticks = np.zeros(6)
-	tickx = np.zeros(5)
-	ticks[1] = nshear*ncl
-	ticks[2] = (nshear+nggl)*ncl
-	ticks[3] = n2pt
-	ticks[4] = n2pt+ncluster
-	ticks[5] = ndata
-
-	cor = np.zeros((ndata,ndata))
-	for i in range(0,ndata):
-		for j in range(0,ndata):
-			if (cov[i,i]*cov[j,j] >0):
-				cor[i,j] = cov[i,j]/math.sqrt(cov[i,i]*cov[j,j])
-
-	fs= 10
-	for i in range(0,5):
-  		tickx[i] = 0.5*(ticks[i]+ticks[i+1])
-  		plt.plot([ticks[i]-0.5,ticks[i]-0.5],[-.5,ndata-0.5],linestyle ='-',color = 'k')
-  		plt.plot([-.5,ndata-0.5],[ticks[i]-0.5,ticks[i]-0.5],linestyle ='-',color = 'k')
-
-	plt.subplot(1, 1, 1)
-	ax = plt.gca()
-	im = ax.imshow(np.log10(cov[:,:]), interpolation="nearest",vmin=-25, vmax=-10)
-	plt.xticks(tickx, labels,fontsize=fs)
-	plt.yticks(tickx-0.5, labels,fontsize=fs)
-	#plt.tick_params(axis = 'x',length = 0, pad = 15)
-	#plt.tick_params(axis = 'y',length = 0, pad = 5)
-
-	plt.colorbar(im)
-	#plt.show()
-	plt.savefig("/home/u17/jiachuanxu/CosmoLike/KL_WFIRST/test_imgs/"+outname[k]+".png", format="png")
-	
-	print ticks
-	
-	# plt.figure()
-	# #plt.imshow(np.log10(cov[0:1500,2000:]), interpolation="nearest",vmin=-25, vmax=-10)
-	# plt.imshow(np.log10(cov[:,:]), interpolation="nearest",vmin=-25, vmax=-10)
-	# #plt.imshow(cor[n2ptcl:n2ptcl+200,300:nshear*ncl], interpolation="nearest",vmax=0.5)
-	# plt.colorbar()
-	# plt.show()
-
-	
 
 
 
